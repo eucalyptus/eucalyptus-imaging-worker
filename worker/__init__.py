688ea5aa94b1792d91a1141dd5f40db628cd9585
Vasiliy Kochergin <vasya@eucalyptus.com>
2014-04-03 17:16:23 -0700

    Don't use hardcoded device name.
    Fixes for: EUCA-9054

--- worker/__init__.py
+++ worker/__init__.py
@@ -26,28 +26,36 @@ from worker.main_loop import WorkerLoop
 import worker.config as config
 import worker
 import subprocess
+import os
 
 __version__ = '1.0.0-dev'
 Version = __version__
 
+def get_block_devices():
+    retlist=[]
+    for filename in os.listdir('/dev'):
+        if any(filename.startswith(prefix) for prefix in ('sd', 'xvd', 'vd', 'xd')):
+            retlist.append('/dev/' + filename)
+    retlist.sort(reverse=True)
+    return retlist
+
 def start_worker():
     if subprocess.call('sudo modprobe floppy > /dev/null', shell=True) != 0:
         log.error('failed to load floppy driver')
-    # The worker is shipped in CentOS hvm image so checking /dev/vdb
-    # veirfy that we are in a VM first so we don't format someone's /dev/vdb
     try:
+        last_dev = get_block_devices()[0]
         worker.config.get_worker_id()
-        if subprocess.call('ls -la /dev/vdb > /dev/null', shell=True) != 0 or subprocess.call('ls -la /mnt > /dev/null', shell=True) != 0:
-            log.error('failed to find /dev/vdb or /mnt')
+        if subprocess.call('ls -la %s > /dev/null' % last_dev, shell=True) != 0 or subprocess.call('ls -la /mnt > /dev/null', shell=True) != 0:
+            log.error('failed to find %s or /mnt' % last_dev)
         else:
             if subprocess.call('sudo mount | grep /mnt > /dev/null', shell=True) == 1:
-                if subprocess.call('sudo mkfs.ext3 /dev/vdb 2>> /tmp/init.log', shell=True) != 0 or subprocess.call('sudo mount /dev/vdb /mnt 2>> /tmp/init.log', shell=True) != 0:
-                    log.error('failed to format and mount /dev/vdb')
+                if subprocess.call('sudo mkfs.ext3 %s 2>> /tmp/init.log' % last_dev, shell=True) != 0 or subprocess.call('sudo mount %s /mnt 2>> /tmp/init.log' % last_dev, shell=True) != 0:
+                    log.error('failed to format and mount %s ' % last_dev)
                 else:
-                    log.info('/dev/vdb was successfully formatted and mounted to /mnt')
+                    log.info('%s was successfully formatted and mounted to /mnt' % last_dev)
             else:
-                log.info('/dev/vdb is alredy mounted to /mnt')
-    except:
-        log.error("Can't detect VM's id")
+                log.info('%s is alredy mounted to /mnt' % last_dev)
+    except Exception, err:
+        log.error("Can't detect VM's id or set up worker due to %s", err)
         sys.exit(1)
     WorkerLoop().start()
