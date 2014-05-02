f7c88d1311867c59babe0d98a0394a2420670142
Vasiliy Kochergin <vasya@eucalyptus.com>
2014-05-02 13:01:10 -0700

    Added failure codes rev 0.1

---
+++ worker/failure_codes.py
@@ -0,0 +1,26 @@
+# Copyright 2009-2014 Eucalyptus Systems, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+# Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+# CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+# additional information or have any questions.
+
+CLEAN_UP_FAILURE = "CleanUpFailure"
+INPUT_DATA_FAILURE = "InvalidInputData"
+DOWNLOAD_DATA_FAILURE = "FailureToDownloadData"
+PROCESS_TASK__FAILURE = "FailureToProcessTask"
+# Can't determine the reason
+GENERAL_FAILURE = "GeneralFailure"
+ATTACH_VOLUME_FAILURE = "FailureToAttachVolume"
+DETACH_VOLUME_FAILURE = "FailureToDetachVolume"
